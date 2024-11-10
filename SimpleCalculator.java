292807ff96cc1c302cfd1ee826e22365858d6212
abdohussien2 <80628811+abdohussien2@users.noreply.github.com>
2024-11-10 11:29:17 +0200

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     long x =scan.nextInt() , y = scan.nextInt();
+ 
+ 
+        System.out.println( x + " + " + y + " = " +(x+y));
+        System.out.println( x + " * " + y + " = " +(x*y));
+        System.out.println( x + " - " + y + " = " +(x-y));
+        }
+ 
+    }
